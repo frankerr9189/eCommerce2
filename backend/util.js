268c3b7f5a7d86ef7d7cc8f2b7f240a765299e05
frankerr9189 <63068800+frankerr9189@users.noreply.github.com>
2020-05-27 07:50:09 -0400

    Update util.js

--- backend/util.js
+++ backend/util.js
@@ -15,7 +15,7 @@ const isAuth = (req, res, next) => {
   
     if (token) {
       const onlyToken = token.slice(7, token.length);
-      jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+      jwt.verify(onlyToken, config.JWT_SECRET, (err, decode)=> {
         if (err) {
           return res.status(401).send({ msg: 'Invalid Token' });
         } 
