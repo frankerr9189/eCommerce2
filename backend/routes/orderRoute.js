5ddeb6d704e19f1aa9bed8d255da2efff1e444eb
frankerr9189 <63068800+frankerr9189@users.noreply.github.com>
2020-05-27 07:48:19 -0400

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,8 +1,28 @@
 import express from 'express';
 import Order from '../models/orderModel';
-import {isAuth} from '../util';
+import {isAuth, isAdmin} from '../util';
 
 const router = express.Router();
+
+router.get("/", isAuth, async (req, res)=> {
+    const orders = await Order.find({}).populate('user');
+    res.send(orders);
+});
+
+router.get("/mine", isAuth, async (req, res)=> {
+    const orders = await Order.find({user: req.user._id});
+    res.send(orders);
+});
+
+router.delete("/:id", isAuth, isAdmin, async (req, res) => {
+    const order = await Order.findOne({_id: req.params.id});
+    if(order){
+        const deletedOrder = await order.remove();
+        res.send(deletedOrder);
+    }else{
+        res.status(404).send("Order Not Found...");
+    }
+});
 
 router.get("/:id", isAuth, async (req, res) => {
     const order = await Order.findOne({_id: req.params.id});
@@ -26,5 +46,25 @@ router.post("/", isAuth, async (req, res) =>{
     });
     const newOrderCreated = await newOrder.save();
     res.status(201).send({message: "New Order Created", data: newOrderCreated});
+});
+
+router.put("/:id/pay", isAuth, async (req, res) =>{
+    const order = await Order.findById(req.params.id);
+    if(order){
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.payment = {
+            paymentMethod: 'paypal',
+            paymentResults:{
+                payerID: req.body.payerID,
+                orderID: req.body.orderID,
+                paymentID: req.body.paymentID
+            }
+        }
+        const updatedOrder = await order.save();
+        res.send({message: 'Order paid.', order: updatedOrder});
+    } else{
+        res.status(404).send({message: 'Order not found.'})
+    }
 });
 export default router;
