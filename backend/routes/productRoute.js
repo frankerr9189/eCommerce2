a7ddff36197c18925bc2b5350d8dfbe3db4ce06c
frankerr9189 <63068800+frankerr9189@users.noreply.github.com>
2020-05-27 07:24:55 -0400

    Update productRoute.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,7 +5,18 @@ import { isAdmin, isAuth} from '../util';
 const router = express.Router();
 
 router.get("/", async (req,res) =>{
-const products = await Product.find({});
+const category = req.query.category ? {category: req.query.category}: {};
+const searchKeyword = req.query.searchKeyword ?{
+    name:{
+        $regex: req.query.searchKeyword,
+        $options: 'i'
+    }
+}: {};
+const sortOrder = req.query.sortOrder ? 
+(req.query.sortOrder === 'lowest'?{price: 1}:{price: -1})
+:
+{_id: -1};
+const products = await Product.find({...category, ...searchKeyword}).sort(sortOrder);
 res.send(products);
 });
 
@@ -18,7 +29,7 @@ router.get("/:id", async (req, res) => {
     }
   });
 
-router.post("/", async (req,res) => {
+router.post("/", isAuth, isAdmin, async (req,res) => {
     const product = new Product({
         name: req.body.name,
         price: req.body.price,
@@ -37,11 +48,10 @@ router.post("/", async (req,res) => {
     return res.status(500).send({message: 'Error in Creating Product.'});
 })
 
-router.put("/:id", async (req,res) => {
+router.put("/:id", isAuth, isAdmin, async (req,res) => {
     const productId = req.params.id;
     const product = await Product.findById(productId);
     if(product){
-
         product.name= req.body.name;
         product.price= req.body.price;
         product.image= req.body.image;
@@ -58,7 +68,7 @@ router.put("/:id", async (req,res) => {
     return res.status(500).send({message: 'Error Updating Product.'});
 });
 
-router.delete("/:id", async(req,res) =>
+router.delete("/:id", isAuth, isAdmin, async(req,res) =>
 {
     const deletedProduct = await Product.findById(req.params.id);
     if(deletedProduct){
