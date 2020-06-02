ec72fb37c49fae9a7a10c823399a004ece54265f
frankerr9189 <63068800+frankerr9189@users.noreply.github.com>
2020-06-02 16:09:52 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,6 @@ app.get("/api/config/paypal", (req, res)=>{
 app.use(express.static(path.join(__dirname, '/../frontend/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
-});
 
-server.listen(port, function() {
-    console.log("App is running on port " + port);
-});
+
+app.listen(config.PORT,...);
